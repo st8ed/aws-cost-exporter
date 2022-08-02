df12bcb8e20c6f2e3d304ca99efc0d9b979b8d05
Kirill Konstantinov <me@st8ed.com>
2022-08-02 08:36:23 +0300

    Release 0.1.5

--- update.sh
+++ update.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 set -ex
 
-VERSION=0.3.3
-CHART_VERSION=0.1.4
+VERSION=0.3.5
+CHART_VERSION=0.1.5
 
 URL_BASE="https://github.com/st8ed/aws-cost-exporter/releases/download/v$VERSION"
 CHART_FILE="aws-cost-exporter-chart-$CHART_VERSION.tgz"
